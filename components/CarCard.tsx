4bcd0481a7d425b9e5a462702326ecde2ffbe28b
Okechukwu Daniel <88836879+Danny-Kyle@users.noreply.github.com>
2023-06-22 19:04:13 +0100

    progress

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client";
 
-const CarCard = () => {
+import { useState } from "react";
+import Image from "next/image";
+
+import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
+import { calculateCarRent } from '../utils/index';
+
+interface carCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({car}: carCardProps) => {
+  const { city_mpg, make, model, transmission, year, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
-    <div>CarCard</div>
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span>
+          {carRent}
+        </span>
+      </p>
+    </div>
   )
 }
 
