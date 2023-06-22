44dc274dec4293dc45da858cccab622ae82105b7
Okechukwu Daniel <88836879+Danny-Kyle@users.noreply.github.com>
2023-06-22 12:03:23 +0100

    api

---
+++ utils/index.ts
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+  params: {model: 'corolla'},
+  headers: {
+    'X-RapidAPI-Key': 'dee0ec99a7msh93670e5bfec89b7p180020jsnc958afefe424',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
